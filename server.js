66220b2a782ca2f174600f1014b820104dde67bf
Влад Язиков <vladzaraza@gmail.com>
2023-10-26 21:04:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,21 @@ const userRouter = require("./router/user_route");
 
 const app = express();
 
+const whitelist = ["https://yazikovxiii.github.io", "http://localhost:5173"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/user", userRouter);
 app.use((err, req, res, next) => {
   const { status = 500, message = "server error" } = err;
